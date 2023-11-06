b4129fd59ad8d2b7ae2d7bafa9be2271aba91643
Andres Hernandez <hernandezboscanandre@gmail.com>
2023-11-06 11:16:22 -0400

    first commit

---
+++ Login.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { GoogleOAuthProvider, GoogleLogin, googleLogout } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+import jwt_decode from 'jwt-decode';
+import { client } from '../client';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const createOrGetuser = async (response) => {
+    const decode = jwt_decode(response.credential);
+    localStorage.setItem('user', JSON.stringify(decode));
+
+    const { name, sub, picture } = decode;
+
+     const doc = {
+      _id: sub,
+      _type: 'user',
+      userName: name,
+      image: picture,
+    };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate('/', { replace: true });
+    });
+  };
+
+  return (
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_PUBLIC_GOOGLE_API_TOKEN}`}>
+      <div className='flex justify-start items-center flex-col h-screen'>
+        <div className='relative w-full h-full'>
+          <video
+            src={shareVideo}
+            type='video/mp4'
+            loop
+            controls={false}
+            muted
+            autoPlay
+            className='h-full w-full object-cover'
+          />
+          <div className='absolute flex flex-col justify-center items-center top-0 left-0 right-0 bottom-0 bg-blackOverlay'>
+            <div className='p-5'>
+              <img src={logo} width='130px' alt='Shareme Logo' />
+            </div>
+            <div className='shadow-2xl'>
+              <GoogleLogin 
+                onSuccess={(response) => createOrGetuser(response)}
+                onError={() => console.log('Error')}
+                cookiePolicy='single_host_origin'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  )
+}
+
+export default Login
